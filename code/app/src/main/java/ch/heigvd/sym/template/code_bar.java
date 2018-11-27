f5e99a5c1280dd2dc83bd89dfe35e7a7414f84cc
Joel Schär <joel.schar@heig-vd.ch>
2018-11-27 16:02:01 +0100

    barcode scan, problem avec le change laylout

--- code/app/src/main/java/ch/heigvd/sym/template/code_bar.java
+++ code/app/src/main/java/ch/heigvd/sym/template/code_bar.java
@@ -1,13 +1,78 @@
 package ch.heigvd.sym.template;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class code_bar extends Activity {
+
+    private Button scan_start;
+    private TextView scanned_value;
+
+    private static final String SELECTED_ITEM_POSITION = "ItemPosition";
+    private int mPosition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_bar);
+
+        scanned_value = findViewById(R.id.scanned_value);
+        scan_start = findViewById(R.id.scan_start);
+
+        scan_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startScann();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Save the state of item position
+        outState.putInt(SELECTED_ITEM_POSITION, mPosition);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(final Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Read the state of item position
+        mPosition = savedInstanceState.getInt(SELECTED_ITEM_POSITION);
+    }
+
+    protected void startScann(){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan a barcode");
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBeepEnabled(true);
+        integrator.initiateScan();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                scanned_value.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
