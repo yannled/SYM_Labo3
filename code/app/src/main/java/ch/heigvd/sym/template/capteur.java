0ad497f321223d3c1c5bf5b6372372797a3e93ca
Yohann Meyer <yohann.meyer@gmail.com>
2018-11-27 14:24:42 +0100

    working v0.1 for capteur part

--- code/app/src/main/java/ch/heigvd/sym/template/capteur.java
+++ code/app/src/main/java/ch/heigvd/sym/template/capteur.java
@@ -1,16 +1,34 @@
 package ch.heigvd.sym.template;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class capteur extends Activity {
+public class capteur extends Activity implements SensorEventListener {
 
     //opengl
     private OpenGLRenderer  opglr           = null;
     private GLSurfaceView m3DView         = null;
+
+    private  SensorManager mSensorManager;
+    private  Sensor mAccelerometer;
+    private  Sensor mMagneticField;
+
+
+    private float[] _R = new float[16];
+    private float[] gravity = new float[3];
+    private float[] geomagnetic = new float[3];
+
+
+    public capteur() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +50,51 @@ public class capteur extends Activity {
         //init opengl surface view
         this.m3DView.setRenderer(this.opglr);
 
+        this.mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        this.mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        this.mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
     }
+
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
 
     /* TODO */
     // your activity need to register accelerometer and magnetometer sensors' updates
+    public void onSensorChanged(SensorEvent event)
+    {
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER) {
+            gravity[0] = event.values[0];
+            gravity[1] = event.values[1];
+            gravity[2] = event.values[2];
+
+        }
+        if (event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD) {
+            geomagnetic[0] = event.values[0];
+            geomagnetic[1] = event.values[1];
+            geomagnetic[2] = event.values[2];
+
+        }
+
+        if(SensorManager.getRotationMatrix(_R, null, gravity, geomagnetic)){
+            _R = this.opglr.swapRotMatrix(_R);
+        }
+
+    }
     // then you may want to call
     //  this.opglr.swapRotMatrix()
     // with the 4x4 rotation matrix, everytime a new matrix is computed
