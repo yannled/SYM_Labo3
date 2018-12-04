ccca621e9a294a26d8b9324acfea8c02fd0288e1
Joel Schär <joel.schar@heig-vd.ch>
2018-12-04 15:00:46 +0100

    ibeacon ok

--- code/app/src/main/java/ch/heigvd/sym/template/MainActivity.java
+++ code/app/src/main/java/ch/heigvd/sym/template/MainActivity.java
@@ -25,10 +25,14 @@
  */
 package ch.heigvd.sym.template;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -41,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
     // For logging purposes
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,5 +100,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+        } else {
+            // Permission has already been granted
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
--- code/app/src/main/java/ch/heigvd/sym/template/ibeacon.java
+++ code/app/src/main/java/ch/heigvd/sym/template/ibeacon.java
@@ -1,13 +1,96 @@
 package ch.heigvd.sym.template;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class ibeacon extends Activity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static android.support.constraint.Constraints.TAG;
+
+// authorisation request :https://developer.android.com/training/permissions/requesting#java
+
+public class ibeacon extends Activity implements BeaconConsumer {
+
+    ListView listView ;
+    List<String> ibeaconList;
+    private BeaconManager beaconManager;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_ibeacon);
+
+
+        listView = findViewById(R.id.ibeacon_list);
+        ibeaconList = new ArrayList<>();
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, ibeaconList);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+        beaconManager.bind((BeaconConsumer) this);
+
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind((BeaconConsumer) this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    ibeaconList.clear();
+                    for(Beacon beacon : beacons){
+                        ibeaconList.add("UUID: " + beacon.getId1()
+                                + "\nMAJOR: " + beacon.getId2()
+                                + "\nMINOR: " + beacon.getId3()
+                                + "\nRSSI: " + beacon.getRssi()
+                                + "\nTX: " + beacon.getTxPower()
+                                + "\nDISTANCE: " + beacon.getDistance());
+                    }
+
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+    }
+
 }
+
